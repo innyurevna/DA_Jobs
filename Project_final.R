7522da9aa8fc20c2c0d92dd3fe6c43468765d354
Семенов Александр Владимирович <aleksandr.vl.semenov@RT.RU>
2020-11-07 17:54:12 +0300

    initial commit

---
+++ Project_final.R
@@ -0,0 +1,46 @@
+library(ggplot2)
+library(dplyr)
+library(tidyr)
+library(stringr)
+df <- as_tibble(read.csv("DataAnalyst.csv"))
+df[df == -1] <- NA
+glimpse(df)
+
+df <- df %>%
+  mutate(Location = gsub(" Arapahoe,", "", Location),
+         Company.Name = gsub("[[:digit:].[:digit:]]", "", Company.Name), 
+         Salary.Estimate = gsub("[\\$Ka-zA-Z\\(.*\\)]", "", Salary.Estimate),
+         Revenue = gsub(" \\(.*\\)", "", Revenue),
+         Job.Title = tolower(Job.Title),
+         Job.Description = tolower(Job.Description)) %>%
+  separate(Salary.Estimate, c("Low_bar.Salary", "High_bar.Salary"), "-", convert = TRUE) %>%
+  separate(Location, c("City", "State"), ",") %>% 
+  mutate_if(is.character, as.factor) %>% 
+  mutate_at(c(2,5), as.character) %>%
+  rename(id = X)
+
+test <- mutate(df, Revenue = gsub(" \\(.*\\)", "", Revenue))
+
+df %>% filter(str_detect(Location, "Arapahoe")) -> case
+
+# Analysis
+
+install.packages("todor")
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+

40fa3b31e26091f9c931c5251a5db2b0541217e1
Inna Krivchenko <innyurevna@gmail.com>
2020-11-18 19:46:22 +0300

    some descriptive ststistics of salary, location and rating

--- Project_final.R
+++ Project_final.R
@@ -3,7 +3,6 @@ library(dplyr)
 library(tidyr)
 library(stringr)
 
-
 df <- as_tibble(read.csv("DataAnalyst.csv"))
 df[df == -1] <- NA
 glimpse(df)
@@ -13,6 +12,7 @@ df <- df %>%
          cname = gsub("[[:digit:].[:digit:]]", "", Company.Name), 
          salary_est = gsub("[\\$Ka-zA-Z\\(.*\\)]", "", Salary.Estimate),
          revenue = gsub(" \\(.*\\)", "", Revenue),
+         rating = as.numeric(Rating),
          job_title = tolower(Job.Title),
          job_desc = tolower(Job.Description)) %>%
   separate(salary_est, c("salary_low", "salary_high"), "-", convert = TRUE) %>%
@@ -22,7 +22,8 @@ df <- df %>%
   rename(id = X)
 
 test <- mutate(df, revenue = gsub(" \\(.*\\)", "", revenue))
-
+test2 <- mutate(df, rating = as.numeric(Rating))
+glimpse(test2)
 df %>% filter(str_detect(location, "Arapahoe")) -> case
 
 # Analysis
@@ -30,8 +31,23 @@ df %>% filter(str_detect(location, "Arapahoe")) -> case
 install.packages("todor")
 # TODO: SOMETHING
 
+#Analysis
 
+#Which jobs are the best judging by salary, company rating and location?
 
+df %>% 
+  group_by(rating) %>% 
+  summarise(salary_low, salary_high, state) %>% 
+  arrange(-rating)
+
+df %>% 
+  group_by(rating) %>% 
+  summarise(salary_low, salary_high, state) %>% 
+  arrange(-salary_high) 
+
+df %>% 
+  group_by(state) %>% 
+  summarise(mean = mean(salary_high, na = T))
 
 
 
